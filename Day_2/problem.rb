f2030752423d0a9170ffd9d29a625b288e38ca28
Alex Zurek <zureka@mail.gvsu.edu>
2018-12-05 08:02:49 -0500

    Day 2 Part 1 complete

--- Day_2/problem.rb
+++ Day_2/problem.rb
@@ -2,30 +2,40 @@ require_relative 'box_ids'
 require 'pry'
 
 class BoxIDChecksum
-  attr_reader :two_letter_count
-  attr_reader :three_letter_count
-  
-  def initialize
-    @letters = ('a'..'z').to_a
+  def self.two_of_any_letter_count(str)
+    find_letters_with_count(str, 2)
   end
 
-  def two_of_any_letter_count(str)
-    result = 0
-    @letters.each do |letter|
-      if (str.count(letter) == 2)
-        result += 1
-      end
-    end
-    result
+  def self.three_of_any_letter_count(str)
+    find_letters_with_count(str, 3)
   end
 
-  def three_of_any_letter_count(str)
-    result = 0
-    @letters.each do |letter|
-      if (str.count(letter) == 3)
-        result += 1
+  def self.count_occurrences_for_collection(collection, count)
+    if count == 2
+      collection.select { |str| two_of_any_letter_count(str) }.count
+    elsif count == 3
+      collection.select { |str| three_of_any_letter_count(str) }.count
+    end
+  end
+
+  def self.get_checksum_for_collection(collection)
+    two_letter_count = count_occurrences_for_collection(collection, 2)
+    three_letter_count = count_occurrences_for_collection(collection, 3)
+
+    two_letter_count * three_letter_count
+  end
+
+  def self.find_letters_with_count(str, count)
+    letters = ('a'..'z').to_a
+
+    letters.each do |letter|
+      if (str.count(letter) == count)
+        return true
       end
     end
-    result
+
+    false
   end
+
+  private_class_method :find_letters_with_count
 end
--- Day_2/problem_test.rb
+++ Day_2/problem_test.rb
@@ -2,47 +2,98 @@ require_relative 'problem'
 require 'minitest/autorun'
 
 describe BoxIDChecksum do
-  before do
-    @subject = BoxIDChecksum.new
-  end
-  
+
   describe "two of any letter count" do
-    it "finds one" do
-      assert_equal 1, @subject.two_of_any_letter_count('aa')
+    it "returns true if letter appears exactly twice" do
+      assert_equal true, BoxIDChecksum.two_of_any_letter_count('aa')
     end
 
-    it "finds none" do
-      assert_equal 0, @subject.two_of_any_letter_count('')
+    it "returns false if letter appears more than twice" do
+      assert_equal false, BoxIDChecksum.two_of_any_letter_count('aaa')
     end
 
-    it "finds none if three of a kind" do
-      assert_equal 0, @subject.two_of_any_letter_count('aaa')
+    it "returns false for empty string" do
+      assert_equal false, BoxIDChecksum.two_of_any_letter_count('')
     end
 
-    it "finds more than one" do
-      assert_equal 3, @subject.two_of_any_letter_count('aabbcc')
+    it "returns true if multiple letters appear exactly twice" do
+      assert_equal true, BoxIDChecksum.two_of_any_letter_count('aabbcc')
     end
   end
 
   describe "three of any letter count" do
-    it "finds one" do
-      assert_equal 1, @subject.three_of_any_letter_count('aaa')
+    it "returns true if letter appears exactly three times" do
+      assert_equal true, BoxIDChecksum.three_of_any_letter_count('aaa')
+    end
+
+    it "returns false if letter appears more than three times" do
+      assert_equal false, BoxIDChecksum.three_of_any_letter_count('aaaa')
+    end
+
+    it "returns false for empty string" do
+      assert_equal false, BoxIDChecksum.three_of_any_letter_count('')
+    end
+
+    it "returns true if multiple letters appear exactly three times" do
+      assert_equal true, BoxIDChecksum.three_of_any_letter_count('aaaccc')
+    end
+  end
+
+  describe "count occurrences for collection" do
+    it "finds one with count of two" do
+      collection = ['aa']
+      assert_equal 1, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+    end
+
+    it "finds two with count of two" do
+      collection = ['aa', 'bb']
+      assert_equal 2, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+    end
+
+    it "finds none with count of two and collection of threes" do
+      collection = ['aaa', 'bbb']
+      assert_equal 0, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+    end
+
+    it "finds two with count of three" do
+      collection = ['aaa', 'bbb']
+      assert_equal 2, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
     end
 
     it "finds none" do
-      assert_equal 0, @subject.three_of_any_letter_count('')
+      collection = []
+      assert_equal 0, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+      assert_equal 0, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
+    end
+  end
+
+  describe "get checksum for collection" do
+    it "returns a 0 checksum with only two-letter occurrences" do
+      collection = ['aa', 'bb', 'cc']
+      assert_equal 3, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+      assert_equal 0, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
+      assert_equal 0, BoxIDChecksum.get_checksum_for_collection(collection)
     end
 
-    it "finds none if two of a kind" do
-      assert_equal 0, @subject.three_of_any_letter_count('aa')
+    it "returns a 0 checksum with only three-letter occurrences" do
+      collection = ['aaa', 'bbb', 'ccc']
+      assert_equal 0, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+      assert_equal 3, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
+      assert_equal 0, BoxIDChecksum.get_checksum_for_collection(collection)
     end
 
-    it "finds none if four of a kind" do
-      assert_equal 0, @subject.three_of_any_letter_count('aaaa')
+    it "returns 1 with one of each type of occurrence" do
+      collection = ['aa', 'bbb']
+      assert_equal 1, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+      assert_equal 1, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
+      assert_equal 1, BoxIDChecksum.get_checksum_for_collection(collection)
     end
 
-    it "finds more than one" do
-      assert_equal 3, @subject.three_of_any_letter_count('aaabbbccc')
+    it "works for the example case from the problem spec" do
+      collection = ['abcdef', 'bababc', 'abbcde', 'abcccd', 'aabcdd', 'abcdee', 'ababab']
+      assert_equal 4, BoxIDChecksum.count_occurrences_for_collection(collection, 2)
+      assert_equal 3, BoxIDChecksum.count_occurrences_for_collection(collection, 3)
+      assert_equal 12, BoxIDChecksum.get_checksum_for_collection(collection)
     end
   end
 end
